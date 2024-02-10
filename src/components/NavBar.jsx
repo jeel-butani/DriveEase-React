71ef2244dcc6f41d97224909042bcf63f0ef3185
jeel-butani <butanijeel1@gmail.com>
2024-02-10 20:55:42 +0530

    ui update

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import '../componentsCss/NavBar.css';
+import { Link } from 'react-router-dom';
+import logo from '../assets/images/logo-vectore.png'
+const Navbar = () =>{
+    return(
+
+        
+        <div class="container highlightTextOut">
+            <div class='logoDiv'> 
+                <img src={logo} className="logo" alt="Vite logo" />
+            </div>
+            <div className='navLinks'>
+                <a alt="HOME">HOME</a>
+                <a alt="ABOUT">ABOUT</a>
+                <a alt="SERVICES">SERVICES</a>
+                <a alt="CONTACT">CONTACT</a>
+            </div>
+            <div className='btnDiv'>
+                <a className='loginBtn' alt="SIGNIN">SIGNIN</a>
+            </div>
+        </div>
+    );
+    
+};
+
+export default Navbar
