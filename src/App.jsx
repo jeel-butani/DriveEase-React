3940a51276007969d36fc8d51a0bdcd6b3e32c50
Hiteshj1693 <Hiteshjethava161611@gmail.com>
2024-02-14 08:02:38 +0530

    app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
 import './App.css'
 import Navbar from './components/NavBar'
-<<<<<<< Updated upstream
-import Home from './pages/home';
-=======
-<<<<<<< HEAD
 import ProductPage from './components/ProductPage';
-=======
-import Home from './pages/home';
->>>>>>> 308f524c3e593f4418d89913a302563385d3cf33
->>>>>>> Stashed changes
 
 function App() {
 
   return (
     <>
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-        
-        <Navbar></Navbar>
-
-=======
         <header>
-        <Navbar/>
+        <Navbar></Navbar>
         </header>
->>>>>>> 308f524c3e593f4418d89913a302563385d3cf33
+        <ProductPage/>
         <div>
-          <Home/>
+          Hello
         </div>
         
     </>
