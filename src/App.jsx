f5b154ea035783c402b2af226fd804b68d56991b
jeel-butani <butanijeel1@gmail.com>
2024-02-23 02:20:52 +0530

    ui update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import Navbar from './components/navBar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
