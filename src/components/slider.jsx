5a42d7ad8e1678b37ba4cddebf7295a6e0bd663f
jeel-butani <butanijeel1@gmail.com>
2024-04-06 11:18:00 +0530

    update

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -5,29 +5,33 @@ import car2 from "../assets/images/car2.jpg";
 import car3 from "../assets/images/car3.jpg";
 const Slider = () => {
   return (
-    <MDBCarousel showControls showIndicators>
-      <MDBCarouselItem itemId={1}>
-        <img
-          src={car1}
-          className="d-block w-100"
-          alt="..."
-        />
-      </MDBCarouselItem>
-      <MDBCarouselItem itemId={2}>
-        <img
-          src={car2}
-          className="d-block w-100"
-          alt="..."
-        />
-      </MDBCarouselItem>
-      <MDBCarouselItem itemId={3}>
-        <img
-          src={car3}
-          className="d-block w-100"
-          alt="..."
-        />
-      </MDBCarouselItem>
-    </MDBCarousel>
+    <MDBCarousel showControls showIndicators className="max-height-500">
+  <MDBCarouselItem itemId={1}>
+    <img
+      src={car1}
+      className="d-block w-100"
+      alt="..."
+      style={{ maxHeight: '500px', objectFit: 'cover' }}
+    />
+  </MDBCarouselItem>
+  <MDBCarouselItem itemId={2}>
+    <img
+      src={car2}
+      className="d-block w-100"
+      alt="..."
+      style={{ maxHeight: '500px', objectFit: 'cover' }}
+    />
+  </MDBCarouselItem>
+  <MDBCarouselItem itemId={3}>
+    <img
+      src={car3}
+      className="d-block w-100"
+      alt="..."
+      style={{ maxHeight: '500px', objectFit: 'cover' }}
+    />
+  </MDBCarouselItem>
+</MDBCarousel>
+
   );
 }
 
