fff222b2482afeeeb5eb373ac5037a6a66090b0b
jeel-butani <butanijeel1@gmail.com>
2024-05-01 07:27:49 +0530

    allow image

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    fs: {
+      // Allow serving files from the specified directories
+      allow: [
+        'D:/Sem_6/drive-ease-backend/assets/carsImage',
+        'D:/Sem_6/AWT/ReactProject/drive-ease/src',
+      ],
+    },
+  },
 })
