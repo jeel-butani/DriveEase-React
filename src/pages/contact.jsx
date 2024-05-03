cd2927cda21113402e0872ceb2f3e32ca15e87a6
jeel-butani <butanijeel1@gmail.com>
2024-05-03 05:32:28 +0530

    change ui for contact form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -15,177 +15,135 @@ const Contact = () => {
   return (
     <>
       <header>
-        <Navbar/>
+        <Navbar />
       </header>
-      <section class="intro-single">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-12 col-lg-8">
-              <div class="title-single-box">
-                <h1 class="title-single">Contact US</h1>
-                <span class="color-text-a">DriveEase Car Rental Company</span>
+      <section class="bg-white" id="contact">
+        <div class="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 lg:py-20">
+          <div class="mb-4">
+            <div class="mb-6 max-w-3xl text-center sm:text-center md:mx-auto md:mb-12">
+              <p class="text-base font-semibold uppercase tracking-wide text-blue-600">
+                Contact
+              </p>
+              <h2 class="font-heading mb-4 font-bold tracking-tight text-gray-900 text-3xl sm:text-5xl">
+                Get in Touch
+              </h2>
+            </div>
+          </div>
+          <div class="flex items-stretch justify-center">
+            <div class="grid md:grid-cols-2">
+              <div class="h-full pr-6">
+                <p class="mt-3 mb-12 text-lg text-gray-600">
+                  Welcome to DriveEase! We're thrilled to have you on board. Whether you're looking to rent a car for your next road trip or a bike for a quick city exploration, we're here to make your experience seamless and enjoyable.
+                </p>
+                <ul class="mb-6 md:mb-0">
+                  <li class="flex">
+                    <div class="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" class="h-6 w-6">
+                        <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
+                        <path
+                          d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z">
+                        </path>
+                      </svg>
+                    </div>
+                    <div class="ml-4 mb-4">
+                      <h3 class="mb-2 text-lg font-medium leading-6 text-gray-900">DriveEase Car Rentals
+                      </h3>
+                      <p class="text-gray-600">1230 Maecenas Street Donec Road</p>
+                      <p class="text-gray-600">New York, EEUU</p>
+                    </div>
+                  </li>
+                  <li class="flex">
+                    <div class="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" class="h-6 w-6">
+                        <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
+                        <path
+                          d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z">
+                        </path>
+                      </svg>
+                    </div>
+                    <div class="ml-4 mb-4">
+                      <h3 class="mb-2 text-lg font-medium leading-6 text-gray-900">DriveEase Bike Rentals
+                      </h3>
+                      <p class="text-gray-600">1230 Maecenas Street Donec Road</p>
+                      <p class="text-gray-600">New York, EEUU</p>
+                    </div>
+                  </li>
+                  <li class="flex">
+                    <div class="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" class="h-6 w-6">
+                        <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2">
+                        </path>
+                        <path d="M15 7a2 2 0 0 1 2 2"></path>
+                        <path d="M15 3a6 6 0 0 1 6 6"></path>
+                      </svg>
+                    </div>
+                    <div class="ml-4 mb-4">
+                      <h3 class="mb-2 text-lg font-medium leading-6 text-gray-900">Contact
+                      </h3>
+                      <p class="text-gray-600">Mobile: +1 (123) 456-7890</p>
+                      <p class="text-gray-600">Mail: tailnext@gmail.com</p>
+                    </div>
+                  </li>
+                  <li class="flex">
+                    <div class="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" class="h-6 w-6">
+                        <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
+                        <path d="M12 7v5l3 3"></path>
+                      </svg>
+                    </div>
+                    <div class="ml-4 mb-4">
+                      <h3 class="mb-2 text-lg font-medium leading-6 text-gray-900">Working
+                        hours</h3>
+                      <p class="text-gray-600">Monday - Friday: 08:00 - 17:00</p>
+                      <p class="text-gray-600">Saturday &amp; Sunday: 08:00 - 12:00</p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div class="card h-fit max-w-6xl p-5 md:p-12" id="form">
+                <h2 class="mb-4 text-2xl font-bold">Ready to Get Started?</h2>
+                <form id="contactForm">
+                  <div class="mb-6">
+                    <div class="mx-0 mb-1 sm:mb-4">
+                      <label for="name" class="pb-1 text-xs uppercase tracking-wider"></label><input
+                        type="text" id="name" autocomplete="given-name" placeholder="Your name"
+                        class="mb-2 w-full bg-white rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md"
+                        name="name" />
+                    </div>
+                    <div class="mx-0 mb-1 sm:mb-4">
+                      <label for="email" class="pb-1 text-xs uppercase tracking-wider"></label><input
+                        type="email" id="email" autocomplete="email"
+                        placeholder="Your email address"
+                        class="mb-2 w-full bg-white rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md"
+                        name="email" />
+                    </div>
+                  </div>
+                  <div class="mx-0 mb-1 sm:mb-4">
+                    <label for="textarea" class="pb-1 text-xs uppercase tracking-wider"></label><textarea
+                      id="textarea" name="textarea" cols="30" rows="5"
+                      placeholder="Write your message..."
+                      class="mb-2 w-full bg-white rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md"></textarea>
+                  </div>
+                  <div class="text-center">
+                    <button type="submit"
+                      class="w-full bg-blue-800 text-white px-6 py-3 font-xl rounded-md sm:mb-0">Send
+                      Message</button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
         </div>
       </section>
-
-      {/*  */}
-
-      <section class="contact">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-7">
-              <form
-                action="forms/contact.php"
-                method="post"
-                role="form"
-                class="php-email-form"
-              >
-                <div class="row">
-                  <div class="col-md-6 mb-3">
-                    <div class="form-group">
-                      <input
-                        type="text"
-                        name="name"
-                        class="form-control form-control-lg form-control-a"
-                        placeholder="Your Name"
-                        required
-                      />
-                    </div>
-                  </div>
-                  <div class="col-md-6 mb-3">
-                    <div class="form-group">
-                      <input
-                        name="email"
-                        type="email"
-                        class="form-control form-control-lg form-control-a"
-                        placeholder="Your Email"
-                        required
-                      />
-                    </div>
-                  </div>
-                  <div class="col-md-12 mb-3">
-                    <div class="form-group">
-                      <input
-                        type="text"
-                        name="subject"
-                        class="form-control form-control-lg form-control-a"
-                        placeholder="Subject"
-                        required
-                      />
-                    </div>
-                  </div>
-                  <div class="col-md-12">
-                    <div class="form-group">
-                      <textarea
-                        name="message"
-                        class="form-control"
-                        cols="45"
-                        rows="8"
-                        placeholder="Message"
-                        required
-                      ></textarea>
-                    </div>
-                  </div>
-                  <div class="col-md-12 my-3">
-                    <div class="mb-3">
-                      <div class="loading">Loading</div>
-                      <div class="error-message"></div>
-                      <div class="sent-message">
-                        Your message has been sent. Thank you!
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="col-md-12 text-center">
-                    <button type="submit" class="btn btn-a">
-                      Send Message
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
-            <div class="col-md-5 section-md-t3">
-              <div class="icon-box section-b2">
-                <div class="icon-box-icon">
-                  <span class="bi bi-envelope"></span>
-                </div>
-                <div class="icon-box-content table-cell">
-                  <div class="icon-box-title">
-                    <h4 class="icon-title">Say Hello</h4>
-                  </div>
-                  <div class="icon-box-content">
-                    <p class="mb-1">
-                      Email :
-                      <span class="color-a">driveease@gmsil.com</span>
-                    </p>
-                    <p class="mb-1">
-                      Phone :
-                      <span class="color-a">+91 9090909090</span>
-                    </p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="icon-box section-b2">
-                  <div class="icon-box-icon">
-                    <span class="bi bi-geo-alt"></span>
-                  </div>
-                  <div class="icon-box-content table-cell">
-                    <div class="icon-box-title">
-                      <h4 class="icon-title">Find us in</h4>
-                    </div>
-                    <div class="icon-box-content">
-                      <p class="mb-1">
-                        Rajkot, Gujarat
-                        <br/> India
-                      </p>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="icon-box">
-                  <div class="icon-box-icon">
-                    <span class="bi bi-share"></span>
-                  </div>
-                  <div class="icon-box-content table-cell">
-                    <div class="icon-box-title">
-                      <h4 class="icon-title">Social networks</h4>
-                    </div>
-                    <div class="icon-box-content">
-                      <div class="socials-footer">
-                        <ul class="list-inline">
-                          <li class="list-inline-item">
-                            <a href="#" class="link-one">
-                              <i class="bi bi-facebook" aria-hidden="true"></i>
-                            </a>
-                          </li>
-                          <li class="list-inline-item">
-                            <a href="#" class="link-one">
-                              <i class="bi bi-twitter" aria-hidden="true"></i>
-                            </a>
-                          </li>
-                          <li class="list-inline-item">
-                            <a href="#" class="link-one">
-                              <i class="bi bi-instagram" aria-hidden="true"></i>
-                            </a>
-                          </li>
-                          <li class="list-inline-item">
-                            <a href="#" class="link-one">
-                              <i class="bi bi-linkedin" aria-hidden="true"></i>
-                            </a>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <Footer/>
+      <Footer />
     </>
   );
 };
